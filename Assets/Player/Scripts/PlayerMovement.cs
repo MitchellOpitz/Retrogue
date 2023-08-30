95494abf15459afa8920f77088f7d99326b7fea7
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-08-30 09:31:09 -0500

    feat - Player movement

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
