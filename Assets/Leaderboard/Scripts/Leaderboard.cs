0bd108f0bf9363c3e06531cbf3cf6aaf41ea78dd
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-09-01 18:46:49 -0500

    feat - LeaderboardCreator connection

---
+++ Assets/Leaderboard/Scripts/Leaderboard.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Dan.Main;
+
+public class Leaderboard : MonoBehaviour
+{
+    public bool leadboardScene;
+
+    [SerializeField]
+    private List<TextMeshProUGUI> highScores;
+
+    private string publicLeaderboardKey =
+        "19ba47951281d2a4fe1120532fc522ca86b9c28a592ca2220f81559bf72b7578";
+
+    private void Start()
+    {
+        if (leadboardScene)
+        {
+            GetLeaderboard();
+        }
+    }
+
+    public void GetLeaderboard()
+    {
+        LeaderboardCreator.GetLeaderboard(publicLeaderboardKey, ((msg) =>
+        {
+            for (int i = 0; i < highScores.Count; i++)
+            {
+                string name = msg[i].Username;
+                string score = msg[i].Score.ToString();
+
+                int nameLength = name.Length;
+                int scoreLength = score.Length;
+                int zerosNeeded = 20 - nameLength - scoreLength;
+
+                string highScoreEntry = name;
+                while (zerosNeeded > 0)
+                {
+                    highScoreEntry = highScoreEntry + " ";
+                    zerosNeeded--;
+                }
+
+                highScoreEntry = highScoreEntry + score;
+
+                highScores[i].text = highScoreEntry;
+            }
+        }));
+    }
+
+    public void SetLeaderboardEntry(string username, int score)
+    {
+        LeaderboardCreator.UploadNewEntry(publicLeaderboardKey, username, score, ((msg) =>
+        {
+            GetLeaderboard();
+        }));
+    }
+}
