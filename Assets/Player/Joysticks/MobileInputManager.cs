6eb875f80f33568c46cf0a8d2f0aadaf88100172
Mitchell Opitz <mitchell@mitchellopitz.net>
2023-10-09 15:43:35 -0500

    fix - Detect mobile OS

--- Assets/Player/Joysticks/MobileInputManager.cs
+++ Assets/Player/Joysticks/MobileInputManager.cs
@@ -6,8 +6,14 @@ public class MobileInputManager : MonoBehaviour
 
     private void Start()
     {
-        // Check if the current platform is a mobile device
-        if (Application.isMobilePlatform)
+        // Determine the current platform's name
+        string currentPlatform = GetPlatformName();
+        Debug.Log("Playing on: " + currentPlatform);
+
+        // Check if the current platform is a mobile device based on its name
+        bool isMobile = IsMobilePlatform(currentPlatform);
+
+        if (isMobile)
         {
             // Enable the mobile joysticks
             mobileJoysticks.SetActive(true);
@@ -17,5 +23,31 @@ public class MobileInputManager : MonoBehaviour
             // Disable the mobile joysticks if not on a mobile device
             mobileJoysticks.SetActive(false);
         }
+    }
+
+    private string GetPlatformName()
+    {
+        // Get the current platform name using the UnityEngine.SystemInfo class
+        return SystemInfo.operatingSystem;
+    }
+
+    private bool IsMobilePlatform(string platformName)
+    {
+        // Check if the platform name contains keywords commonly found in mobile devices
+        platformName = platformName.ToLower();
+
+        // List of common mobile platform keywords
+        string[] mobileKeywords = { "android", "iphone", "ipad", "ios", "mobile" };
+
+        // Check if any of the keywords are found in the platform name
+        foreach (string keyword in mobileKeywords)
+        {
+            if (platformName.Contains(keyword))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
